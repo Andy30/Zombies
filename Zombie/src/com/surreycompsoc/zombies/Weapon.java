0728cfd91058b3f55a3b28b949cccd9e86ccc723
Christopher Kilding <ck00071@surrey.ac.uk>
2011-04-06 17:37:10 +0100

    Added skeleton classes to illustrate what I'm thinking with the Component and Factory patterns.

---
+++ Zombie/src/com/surreycompsoc/zombies/Weapon.java
@@ -0,0 +1,15 @@
+/**
+ * Weapon.java
+ */
+
+package com.surreycompsoc.zombies;
+
+
+/**
+ * Generic container class which "contains" a bunch of behaviour that specifies how a Weapon works. E.g. Clip, Damage etc.
+ * 
+ * @author Christopher Kilding
+ */
+public class Weapon {
+
+}
---
+++ Zombie/src/com/surreycompsoc/zombies/WeaponFactory.java
@@ -0,0 +1,15 @@
+/**
+ * WeaponFactory.java
+ */
+
+package com.surreycompsoc.zombies;
+
+
+/**
+ * Static factory which handles the assembly of the components of various Weapons, then returns the finished Weapon without you, the programmer, having to worry about passing things into constructors etc.
+ * 
+ * @author Christopher Kilding
+ */
+public class WeaponFactory {
+
+}
