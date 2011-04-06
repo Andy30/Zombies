4fdb7b8018aabd833b35474c04ec46579dff6d48
Christopher Kilding <ck00071@surrey.ac.uk>
2011-04-06 19:10:18 +0100

    Refactored Weapons to have Images of themselves, did various cleanup operations around the other modified classes.

---
+++ Zombie/src/com/surreycompsoc/zombies/Magazine.java
@@ -0,0 +1,80 @@
+/**
+ * Magazine.java
+ */
+
+package com.surreycompsoc.zombies;
+
+/**
+ * State class modelling a Weapon's Magazine, which holds some rounds.
+ * 
+ * @author Christopher Kilding
+ */
+public class Magazine {
+
+  /**
+   * Max num of rounds that the Magazine can hold at once. Once set, this can obviously never change - that would be physically
+   * impossible.
+   */
+  private final int maxSize;
+  /** The current num of rounds in the Magazine. */
+  private int       currentSize;
+
+  /**
+   * Constructor for objects of class Magazine. We assume that the Magazine starts fully loaded.
+   * 
+   * @param maxSize
+   *          the num of rounds that the Magazine can hold when full
+   */
+  public Magazine(int maxSize) {
+    this.maxSize = maxSize;
+    this.currentSize = maxSize;
+  }
+
+  /**
+   * Depletes the number of rounds in the Magazine by 1.
+   * @return whether firing was successful (i.e. whether there were still enough rounds in the Magazine)
+   */
+  public boolean fire() {
+    boolean success = false;
+    
+    if (this.currentSize > 0) {
+      this.currentSize--;
+      success = true;
+    }
+    
+    return success;
+  }
+
+  /**
+   * Reload the Magazine to full capacity. It's up to the caller to reduce the number of extra rounds by the relevant amount - if this
+   * is your desired behaviour.
+   */
+  public void reload() {
+    this.currentSize = this.maxSize;
+  }
+
+  /**
+   * Partially reload the Magazine by adding the specified number of rounds to it. Would most likely be used if you had little ammo left
+   * and couldn't do a complete restock. If you attempt to pass in some massive amount of extra rounds that would overstuff the
+   * Magazine, or if you attempt to pass in zero or a negative number of rounds, the method fails and returns false.
+   * 
+   * @param amount
+   *          the number of rounds to add to the Magazine
+   * @return whether the reload operation could be completed
+   */
+  public boolean reload(int amount) {
+    boolean success = false;
+
+    // Can't reload with fewer than 1 round - that would be pointless.
+    if (amount > 0) {
+      int newNum = this.currentSize += amount;
+
+      // Can only reload the Magazine with the specified amount if it can physically hold the new number of rounds.
+      if (newNum <= this.maxSize) {
+        this.currentSize = newNum;
+        success = true;
+      }
+    }
+    return success;
+  }
+}
--- Zombie/src/com/surreycompsoc/zombies/Weapon.java
+++ Zombie/src/com/surreycompsoc/zombies/Weapon.java
@@ -4,42 +4,52 @@
 
 package com.surreycompsoc.zombies;
 
+import java.awt.Image;
 import java.util.Random;
 
 /**
- * Generic container class which "contains" a bunch of behaviour that specifies how a Weapon works. E.g. Clip, Damage etc.
+ * Generic container class which "contains" a bunch of behaviour that specifies how a Weapon works. E.g. Magazine, Damage etc.
  * 
  * @author Christopher Kilding
  */
 public class Weapon {
 
-  /** The Clip containing rounds that can be fired off. */
-  private Clip clip;
+  /** The Magazine containing rounds that can be fired off. */
+  private Magazine  clip;
 
-  /** The extra rounds a Weapon has which aren't loaded into the Clip at the time. */
-  private int  stockRounds;
+  /** The extra rounds a Weapon has which aren't loaded into the Magazine at the time. */
+  private int   stockRounds;
 
   /**
    * The base damage done per round. This changes slightly every time the weapon is fired with a random number to simulate the
    * varying degrees of accuracy without the need to model individual rounds with bounding boxes. This is the maximum amount of
    * damage a standard round can do if it were to land a critical hit on the target.
    */
-  private int  baseDamagePerRound;
+  private int   baseDamagePerRound;
+
+  /**
+   * Simplistic Image of the Weapon. This won't change based on the angle - no 3D simulation here - just a plain 2D image that is
+   * rotated with the player.
+   */
+  private Image img;
 
   /**
    * Constructor for objects of class Weapon.
    * 
    * @param clip
-   *          the conceptual model of the Clip for this Weapon
+   *          the conceptual model of the Magazine for this Weapon
    * @param stockRounds
    *          the number of extra rounds that the Weapon will start with
    * @param baseDamagePerRound
    *          the maximum amount of damage done by a standard round
+   * @param img
+   *          single Image of the weapon, viewed top-down
    */
-  public Weapon(Clip clip, int stockRounds, int baseDamagePerRound) {
+  public Weapon(Magazine clip, int stockRounds, int baseDamagePerRound, Image img) {
     this.clip = clip;
     this.stockRounds = stockRounds;
     this.baseDamagePerRound = baseDamagePerRound;
+    this.img = img;
   }
 
   /**
@@ -48,14 +58,14 @@ public class Weapon {
    * @return the amount of damage done by the round that was fired
    */
   public double fire() {
-    // Attempt to deplete 1 round from the Clip
+    // Attempt to deplete 1 round from the Magazine
     boolean couldFire = this.clip.fire();
     // The variable containing how much damage the round will do.
     // (If there's no rounds left, the damage defaults to zero.)
     double damageDealt = 0;
 
-    // If there was ammo in the Clip, fire() would have returned true. Continue with the firing behaviour.
-    // If not, the Clip was empty, so stop now.
+    // If there was ammo in the Magazine, fire() would have returned true. Continue with the firing behaviour.
+    // If not, the Magazine was empty, so stop now.
     if (couldFire == true) {
       // Generate random number to simulate the loss in damage effectiveness that would be caused by a round that hit a non-critical
       // location / bounced off armour instead of landing a critical hit.
@@ -65,5 +75,12 @@ public class Weapon {
     }
 
     return damageDealt;
+  }
+
+  /**
+   * @return the Weapon's Image
+   */
+  public Image getImage() {
+    return this.img;
   }
 }
