812e24469589605f19124dd5e6e7a6eda94961de
Christopher Kilding <ck00071@surrey.ac.uk>
2011-04-06 18:02:56 +0100

    Refactored Clip to have a maxSize (set once) and a currentSize, which allows for reloading.

--- Zombie/src/com/surreycompsoc/zombies/Clip.java
+++ Zombie/src/com/surreycompsoc/zombies/Clip.java
@@ -4,28 +4,42 @@
 
 package com.surreycompsoc.zombies;
 
-
 /**
  * @author chris
  */
 public class Clip {
 
-  /** Max num of rounds that the Clip can hold at once. */
-  private int size;
-  
   /**
-   * Constructor for objects of class Clip.
-   * 
-   * @param size set the num of rounds that the Clip can hold, when full
+   * Max num of rounds that the Clip can hold at once. Once set, this can obviously never change - that would be physically
+   * impossible.
    */
-  public Clip(int size) {
-    this.size = size;
+  private final int maxSize;
+  /** The current num of rounds in the Clip. */
+  private int       currentSize;
+
+  /**
+   * Constructor for objects of class Clip. We assume that the Clip starts fully loaded.
+   * 
+   * @param maxSize
+   *          the num of rounds that the Clip can hold when full
+   */
+  public Clip(int maxSize) {
+    this.maxSize = maxSize;
+    this.currentSize = maxSize;
   }
-  
+
   /**
    * Depletes the number of rounds in the Clip by 1.
    */
   public void fire() {
-    this.size--;
+    this.currentSize--;
+  }
+
+  /**
+   * Reload the Clip to full capacity. It's up to the caller to reduce the number of extra rounds by the relevant amount - if this
+   * is your desired behaviour.
+   */
+  public void reload() {
+    this.currentSize = this.maxSize;
   }
 }
